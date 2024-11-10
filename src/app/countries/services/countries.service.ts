c36540c1ae6e32640214be58d2ba65aa09176e87
Alejandro Bece <alejandro.becerra.pro@gmail.com>
2024-11-10 11:48:43 -0500

    manejo de errores en observables - catcherror

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,10 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({ providedIn: 'root' })
-
 export class CountriesService {
   private apiUrl: string = 'https://restcountries.com/v3.1';
 
@@ -20,6 +19,8 @@ export class CountriesService {
   // }
 
   searchCapital(value: string): Observable<Country[]> {
-    return this.http.get<Country[]>(`${this.apiUrl}/capital/${value}`);
+    return this.http
+      .get<Country[]>(`${this.apiUrl}/capital/${value}`)
+      .pipe(catchError(() => of([])));
   }
 }
